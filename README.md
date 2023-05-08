e965f4ef425b5e4dc95eee36511c567f6a265702
Gino Luraschi <gluraschi@iqhive.com>
2023-05-08 16:27:03 +1200

    feat: Add wrap to accept API Gateway request and response

--- README.md
+++ README.md
@@ -1,5 +1,34 @@
 # Golang API with OpenAPI and AWS Lambda
 
+## Steps
+
+### Create Golang API and AWS layer
+We need to implement the AWS Proxy event request and response:
+```go
+func (h Handler) HandleRequest(_ context.Context, event events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error)
+```
+Add API_TOKEN and API_ORG variables into Lambda
+
+### Add API Gateway
+1. Create new REST API
+2. Name and regional endpoint
+3. Create new resource, just set name and url endpoint
+4. Select resource then go to `actions` -> `create method`
+5. When create method select integration Lambda, check use lambda proxy integration select our lambda then save
+
+## Deploy API Gateway
+1. Go to `actions` --> `deploy API`
+2. Select new stage with name test (or whatever you want)
+
+## Test API
+```json
+{
+  "Value": "Cual es el mejor equipo de futbol de Argentina?"
+}
+```
+
+### Create Frontend
+
 ### Resources:
 * [Writing and deploying your first AWS Lambda with Go](https://levelup.gitconnected.com/writing-and-deploying-your-first-aws-lambda-7a6d5800b443)
 * [Consume Open AI (ChatGPT) With Golang](https://betterprogramming.pub/chatgpt-golang-a7bd524e7563)
--- cmd/lambda/main.go
+++ cmd/lambda/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+
 	"go-openapi-consultor/internal/actions"
 	"go-openapi-consultor/internal/client"
 	"go-openapi-consultor/internal/domain"
-	"os"
 )
 
 func main() {
@@ -17,18 +22,77 @@ func main() {
 			Chat: client.NewClient(apiKey, apiOrg),
 		},
 	}
-
-	lambda.Start(handler.HandleRequest)
+	lambda.Start(handler.wrapper)
 }
 
 type Handler struct {
 	sender actions.SendMessage
 }
 
-func (h Handler) HandleRequest(_ context.Context, message domain.Message) ([]domain.ChatResult, error) {
+type ErrorResponse struct {
+	Message string `json:"message"`
+}
+
+type Response struct {
+	Result        []domain.ChatResult `json:"result,omitempty"`
+	ErrorResponse *ErrorResponse      `json:"error,omitempty"`
+}
+
+func (h Handler) wrapper(event events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	if len(event.Body) == 0 {
+		err := fmt.Errorf("bad formatted body")
+		return convertResponse(nil, http.StatusBadRequest, err), nil
+	}
+	messageFrom, err := h.getMessageFrom(event)
+	if err != nil {
+		return convertResponse(nil, http.StatusBadRequest, err), nil
+	}
+	response, err := h.execute(messageFrom)
+	if err != nil {
+		return convertResponse(nil, http.StatusInternalServerError, err), nil
+	}
+	return convertResponse(response, http.StatusOK, err), nil
+}
+
+func (h Handler) getMessageFrom(event events.APIGatewayProxyRequest) (domain.Message, error) {
+	msg := domain.Message{}
+	err := json.Unmarshal([]byte(event.Body), &msg)
+	if err != nil {
+		return domain.Message{}, err
+	}
+	if len(msg.Value) == 0 {
+		return domain.Message{}, fmt.Errorf("the message value is required")
+	}
+	return msg, nil
+}
+
+func (h Handler) execute(message domain.Message) ([]domain.ChatResult, error) {
+	if len(message.Value) == 0 {
+		return nil, fmt.Errorf("the message value is required")
+	}
 	result, err := h.sender.Chat.Chat(message)
 	if err != nil {
 		return nil, err
 	}
 	return result, nil
+}
+
+func convertResponse(response []domain.ChatResult, statusCode int, err error) *events.APIGatewayProxyResponse {
+	var resp Response
+	if err == nil {
+		resp.Result = response
+	} else {
+		resp.ErrorResponse = &ErrorResponse{Message: err.Error()}
+	}
+	bytes, err := json.Marshal(resp)
+	if err != nil {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body:       "{\"error\": \"" + err.Error() + "\"}",
+		}
+	}
+	return &events.APIGatewayProxyResponse{
+		StatusCode: statusCode,
+		Body:       string(bytes),
+	}
 }
